bbb6e6602dcb0baf05ab66b49c74f51d1eb8a1bd
hemesipp <mathieu.simon@telecom-sudparis.eu>
2021-03-26 11:44:23 +0100

    Test loop working

--- cluster/kafka/src/pacman/pacman.py
+++ cluster/kafka/src/pacman/pacman.py
@@ -46,10 +46,6 @@ def job_handler(name: str):
     r = random()
     if r < 0.1:
         nb_cons_wanted = randint(1, 10)
-    print(act_cons_list)
-    print(nb_cons_wanted)
-    print(last_cons_id)
-    print(len(act_cons_list))
     if nb_cons_wanted < len(act_cons_list):
         real_name=name.rstrip('\n')
         i = int(real_name[-1:])
@@ -64,21 +60,6 @@ def job_handler(name: str):
 
 
 
-"""
-main est appelée à chaque nouvelle requete reçu
-
-waiting_list: liste de new_consumer en attente
-nb_container_asked: valeur rendue par l'algo de prédiction
-nb_container: nombre de pods actifs
-"""
-
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
-    """
-    nb_container = 0
-    While True:
-        #How to wait a request and add to waiting_list?
-        nb_container_asked = algo(param1, param2, nb_container)         
-        main(nb_container_asked, nb_container, waiting_list)
-    """
+
