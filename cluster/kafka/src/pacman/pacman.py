bb3cca6a8ab204b61db0f4a8312e8532ac748557
hemesipp <mathieu.simon@telecom-sudparis.eu>
2021-03-26 11:09:59 +0100

    algo pacman working

--- cluster/kafka/src/pacman/pacman.py
+++ cluster/kafka/src/pacman/pacman.py
@@ -1,45 +1,64 @@
 from fastapi import FastAPI
-import os
 import uvicorn
 from random import *
-#import add_new_consumer.py
-#from kubernetes import client, config
+from kubernetes import client, config
 
 # import time
-"""
+
 def remove_pod(name):
-    config.load_kube_config()
+    config.load_incluster_config()
 
     api_instance = client.CoreV1Api()
 
     namespace = 'default'  # str
 
     return api_instance.delete_namespaced_pod(name, namespace)
-"""
+
+def create_pod(id):
+    config.load_incluster_config()
+
+    v1 = client.CoreV1Api()
+
+    pod = client.V1Pod()
+    pod.metadata = client.V1ObjectMeta(name="new-consumer-" + str(id))
+
+    container = client.V1Container(name="new-consumer-" + str(id), image="new-consumer:latest", image_pull_policy="Never")
+
+    spec = client.V1PodSpec(containers=[container], restart_policy="Always")
+    pod.spec = spec
+
+    return v1.create_namespaced_pod(namespace="default", body=pod)
+
+
 
 app = FastAPI()
 
 
+act_cons_list = [1, 13, 21]
+nb_cons_wanted = 4
+last_cons_id = 23
+
 @app.get("/{name}")  # id of consumer in entry
 def job_handler(name: str):
-    act_cons_list = [1, 13, 21]
-    nb_cons_wanted = 1
-    last_cons_id = 23
-    if nb_cons_wanted < len(act_cons_list):
-        os.system("python3 remove_new_consumer.py")
-    else:
-        return {"message": "first_job_id"}
     """
     r = random()
-    if r < 0.1:
+    if r < 0.5:
         nb_cons_wanted = randint(1, 10)
+    """
+    global last_cons_id
+    print(act_cons_list)
+    print(nb_cons_wanted)
+    print(last_cons_id)
+    print(len(act_cons_list))
+    if nb_cons_wanted < len(act_cons_list):
+        return remove_pod(name.rstrip('\n'))
+    else:
         while nb_cons_wanted > len(act_cons_list):
             last_cons_id += 1
-            add_new_consumer.create_pod(last_cons_id)
             act_cons_list.append(last_cons_id)
-    # main()
-    """
-    return {"name": name}
+            create_pod(last_cons_id)
+        return {"message": "first_job_id"}
+
 
 
 """
@@ -50,18 +69,6 @@ nb_container_asked: valeur rendue par l'algo de prédiction
 nb_container: nombre de pods actifs
 """
 
-"""
-def main(nb_container_asked, nb_container, waiting_list):
-    while True:
-        while nb_container_asked<=nb_container:
-            remove_new_consumer(waiting_list[0])
-            nb_container-=1
-        while nb_container_asked>=nb_container:
-            add_new_consumer(nb_container_asked)
-            nb_container+=1
-        if nb_container_asked==nb_container:
-            #Send packet
-"""
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
