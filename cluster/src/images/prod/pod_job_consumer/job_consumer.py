PID scaling policy and exponential distribution inter-request policy added. Some other fixes has been added too

cluster/src/images/prod/pod_job_consumer/job_consumer.py
@@ -7,8 +7,10 @@ from random import *
 
 if __name__ == "__main__":
     while True:
-        w = random()
-        t = 10*np.exp(w)
+        #w = random()
+        #t = 10*np.exp(w)
+        b = 5.0
+        t = np.random.exponential(b)
         time.sleep(t)
 
         bashCommandName = 'echo $HOSTNAME'

cluster/src/images/prod/producer/producer.py
@@ -25,13 +25,15 @@ class Producer(threading.Thread):
             self.run()
 
         while not self.stop_event.is_set():
-            producer.send('topic_1', ("ciao" + str(i)).encode())  # Send a job in the kafka queue
+ # Send a job in the kafka queue
             offset = i + 1
             requests.get("http://pacman:80/metrics/" + str(offset))  # Request pacman to tell him that a new job is in the queue
-            print("I am alive boys")
+            producer.send('topic_1', ("ciao" + str(i)).encode())
             i += 1
-            w = random()
-            t = np.exp(w)
+            #w = random()
+            #t = np.exp(w)
+            b = 1.0
+            t = np.random.exponential(b)
             time.sleep(t)
 
         producer.close()